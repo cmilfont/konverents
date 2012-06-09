e26201ac4bb8591108714d655dd1e7edc66906f2
Christiano Milfont <cmilfont@gmail.com>
2012-06-09 13:21:59 -0300

    [#6] algumas telas

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,13 +1,30 @@
 # encoding: UTF-8
 class TracksController < ApplicationController
 
+  before_filter :find_event
   respond_to :html
 
   def index
-    debugger
-    @event = Event.find params[:event_id]
     @tracks = @event.tracks
     respond_with @tracks
+  end
+  
+  def show
+    @track = Track.find params[:id]
+  end
+  
+  def new
+    @track = Track.new :event_id => @event
+  end
+  
+  def create
+    @track = Track.new params[:track]
+    @track.save
+    respond_with @track
+  end
+  
+  def find_event
+    @event = Event.find params[:event_id]
   end
 
 end
