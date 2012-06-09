0d9652de12aa47fdff7b7b565d8e11783d814660
Christiano Milfont <cmilfont@gmail.com>
2012-06-09 09:41:38 -0300

    primeiro commit lindão com o rêius

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konverents::Application.config.secret_token = '18dcefe29cf363ce466eb0f7c178c7719a78adffb94428544352d515ead2937c448ea642f78fe86c60d04062d880004421297af0f4260e4c4fc3babd27aca9ff'
