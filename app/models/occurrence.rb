771c3cf0a4bf31449a8f49494d682fca469799c7
Dev2 <dev2@milfont.org>
2012-06-21 21:55:44 -0300

    testando o model occurrence

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,3 +1,4 @@
 class Occurrence < ActiveRecord::Base
   belongs_to :track
+  validates_presence_of :hour, :description
 end
--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -3,8 +3,22 @@ require 'spec_helper'
 
 describe Occurrence do
 
+  describe "Validations" do
+    it { should validate_presence_of :hour }
+    it { should validate_presence_of :description }
+  end
+
   describe "relationships" do
     it { should belong_to :track }
   end
   
+  describe "palestra" do
+    before do
+      @palestra = mock_model("Palestra", :id => 23, :description => "Palestra RoR")
+      @occurence = mock_model(Occurrence, :id => 1, :hour => "17:00", :description => "...")
+    end
+    it "occurrence should be a palestra" do
+      @occurrence.stub(:palestra).and_return(@palestra.description).should == "Palestra RoR"
+    end
+  end
 end
