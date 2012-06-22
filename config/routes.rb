4b32e651ad55cba95a7f8697a6415d0e3d1e9841
Christiano Milfont <cmilfont@gmail.com>
2012-06-22 09:40:39 -0300

    retirei o bloco do devise_for como pedia nos warnings

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Konverents::Application.routes.draw do
 
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
-    get "sign_in",  :to => "devise/sessions#new", :as => :login
-    get "sign_up",  :to => "devise/registrations#new"
-    get "sign_out", :to => "devise/sessions#destroy", :as => :logout  
-  end
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
     get '/users/auth/facebook' => 'users/omniauth_callbacks#passthru'
     get '/users/auth/facebook/callback' => 'users/omniauth_callbacks#passthru'
+    get "sign_in",  :to => "devise/sessions#new", :as => :login
+    get "sign_up",  :to => "devise/registrations#new"
+    get "sign_out", :to => "devise/sessions#destroy", :as => :logout
   end
 
   root :to => 'home#index'
